b06617f1825b94cc56616af174dfe205c00c7b46
manjunathbm2703 <manjunathbm2003@gamil.com>
2026-01-02 20:09:32 +0530

    3. Spring Security Setup

--- ecommerce/src/main/java/com/jsp/ecommerce/repository/UserRepository.java
+++ ecommerce/src/main/java/com/jsp/ecommerce/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.jsp.ecommerce.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.jsp.ecommerce.entity.User;
@@ -7,5 +9,8 @@ import com.jsp.ecommerce.entity.User;
 public interface UserRepository extends JpaRepository<User, Integer> {
  
 	Boolean existsByEmail(String adminEmail);
+
+	
+	Optional<User> findByEmail(String email);
 	
 }
